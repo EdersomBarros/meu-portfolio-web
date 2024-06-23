74091dbedd4bbddcf3a0d07f4d65ba742d46fb2c
Edersom Barros <edersombarros@hotmail.com>
2024-06-23 15:43:20 -0400

    finalizado projeto

---
+++ main.js
@@ -0,0 +1,6 @@
+document.getElementById("profilePic")
+            .addEventListener("click", 
+                    function () {
+                        this.classList
+                        .toggle("rotating");
+});
